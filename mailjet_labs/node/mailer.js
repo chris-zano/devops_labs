21df454540a095dc3febc61a4a48a8f1af69cd58
Christian Solomon <cncs.chris@gmail.com>
2024-11-12 10:11:44 +0000

    restart

---
+++ mailjet_labs/node/mailer.js
@@ -0,0 +1,54 @@
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const fs = require('fs').promises;
+const path = require('path');
+dotenv.config()
+
+const getSystemCredentials = () => {
+    return {
+        user: process.argv[2],
+        pass: process.argv[3]
+    }
+}
+
+const filepath = path.join(__dirname, '..', 'system_alert_email.json');
+
+try {
+
+    async function readFileData() {
+        try {
+            const data = await fs.readFile(filepath, 'utf-8');
+            const jsonData = JSON.parse(data);
+
+            const { subject, message } = jsonData;
+            const { user, pass } = getSystemCredentials()
+
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: { user, pass }
+            });
+
+            const options = {
+                from: user,
+                to: 'niico.ncs@gmail.com',
+                subject: subject,
+                html: `<p>${message}</p>`
+            }
+
+            transporter.sendMail(options)
+                .then(response => {
+                    console.log('node mailer sent message with a status of')
+                })
+                .catch(error => {
+                    throw new Error(error);
+                })
+        } catch (err) {
+            console.error('Error reading the file:', err);
+        }
+    }
+
+    readFileData();
+
+} catch (error) {
+    console.error('an error occured:', error);
+}
